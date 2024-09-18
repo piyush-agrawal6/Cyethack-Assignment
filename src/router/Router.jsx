ec89cf6a7199a66df6d1aa647fde78fe1029bd3a
Piyush Agrawal <agrawaljoy1@gmail.com>
2024-09-18 18:52:38 +0530

    bugs fixed

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "../pages/Signup/Signup";
-import Navbar from "../components/Navbar/Navbar";
-import List from "../Pages/List/List";
-import ListDetails from "../pages/Details/ListDetails";
 import Dashboard from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
+import Navbar from "../components/Navbar/Navbar";
+import List from "../pages/List/List";
+import ListDetails from "../pages/Details/ListDetails";
+import Signup from "../pages/Signup/Signup";
 
 const Router = () => {
   return (
@@ -13,8 +13,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route index element={<Dashboard />} />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="*" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Dashboard />} />
           <Route path="/list" element={<List />} />
           <Route path="/list/details" element={<ListDetails />} />
         </Route>
